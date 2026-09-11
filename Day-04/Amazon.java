56b809c26ce950a4d67de8f3d15903417d5dff6a
Ravi Baishya <321843864+ravibaishya@users.noreply.github.com>
2026-09-11 23:57:41 +0530

    Add Day 04 Java examples and practice exercises

---
+++ Day-04/Amazon.java
@@ -0,0 +1,15 @@
+class Amazon
+{
+	public static void main(String args[])
+	{
+	
+		String orderId = args[0];
+		String productName = args[1];
+		String orderPlacedBy = args[2];
+		String deliveryAddress = args[3];
+		System.out.println(orderId);
+		System.out.println(productName);
+		System.out.println(orderPlacedBy);
+		System.out.println(deliveryAddress);
+	}
+}
---
+++ Day-04/CreditCardData.java
@@ -0,0 +1,13 @@
+class CreditCardData
+{
+	public static void main(String args[])
+	{
+	
+		String mall1 = args[0];
+		String mall2 = args[1];
+		String mall3 = args[2];
+		System.out.println("We are analysing " + mall1 +" Data.");
+		System.out.println("We are processing " + mall2 +" Data.");
+		System.out.println("We are proceeding your " + mall3 +" Data.");
+	}
+}
